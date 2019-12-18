683fd661c6688f1697aad299b13c4ab105518eb5
Andres Avila <andres.avila@netpay.com.mx>
2019-12-18 17:12:50 -0600

    first

---
+++ TestingSDK/TestingSDK/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  TestingSDK
+//
+//  Created by Andres Avila on 12/18/19.
+//  Copyright © 2019 Andres Avila. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init() {}
+    
+    public static func printSomething()-> String {
+        return "success"
+    }
+    
+}
---
+++ TestingSDK/TestingSDK/TestingSDK.h
@@ -0,0 +1,19 @@
+//
+//  TestingSDK.h
+//  TestingSDK
+//
+//  Created by Andres Avila on 12/18/19.
+//  Copyright © 2019 Andres Avila. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestingSDK.
+FOUNDATION_EXPORT double TestingSDKVersionNumber;
+
+//! Project version string for TestingSDK.
+FOUNDATION_EXPORT const unsigned char TestingSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestingSDK/PublicHeader.h>
+
+
